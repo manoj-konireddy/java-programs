177b74eb8ecba9520a1226d56885e9a2ec1cdd70
manoj-konireddy <manojkumarreddy21042004@gmail.com>
2025-06-09 12:04:55 +0530

    Update Find the Town Judge.java

--- Find the Town Judge.java
+++ Find the Town Judge.java
@@ -2,14 +2,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Solution sol = new Solution();
-        
-        // Example input: n = 3, trust array
-        int n = 3;
-        int[][] trust = {{1,3},{2,3}};
-        
+
+        System.out.print("Enter number of people (n): ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter number of trust relationships: ");
+        int m = sc.nextInt();
+
+        int[][] trust = new int[m][2];
+        System.out.println("Enter trust pairs (a b) meaning 'a trusts b':");
+        for(int i = 0; i < m; i++){
+            trust[i][0] = sc.nextInt();
+            trust[i][1] = sc.nextInt();
+        }
+
         int judge = sol.findJudge(n, trust);
         System.out.println("The judge is: " + judge);
+        sc.close();
     }
 }
 
